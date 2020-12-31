92e734a045e09b58b15c42a9ef4b111b4786f193
barjan <bardugo1@protonmail.com>
2021-01-01 01:42:13 +0200

    added a headin

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ function App() {
   const [choosedActors, setChoosedActors] = useState([]);
 
   return (
-    <div>
-      <h2>באפליקציה הזו</h2>
-      <h3>כל הקלדה שתכניסו לאינפוט</h3>
-      <h3>תפלטר ותציג רק את השחקנים המתאימים לפי השמות הפרטיים ו/או משפחה</h3>
-      <h3>וכמו כן, רק את הסרטים מתוך הגייסו, שמופיעים בהם השחקנים המוצגים בדף לאחר הפילטור</h3>
+    <div className="app-div">
+      <div className="flag-text">
+        <div className="effect">
+          <h3 className="txtwav slow">באפליקציה הזו</h3>
+          <h3 className="txtwav slow">כל הקלדה שתכניסו לאינפוט</h3>
+          <h3 className="txtwav slow">תפלטר ותציג רק את השחקנים המתאימים לפי השמות הפרטיים ו/או משפחה</h3>
+          <h3 className="txtwav slow">וכמו כן, רק את הסרטים מתוך הגייסו, שמופיעים בהם השחקנים המוצגים בדף לאחר הפילטור</h3>
+        </div>
+          
+      </div>
+ 
       <ActorPage movieByActor={setChoosedActors} />
       <MoviesPage show={choosedActors}/>
     </div>
